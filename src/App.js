08fc2741788b29c6b32d7a1fa9d015e6281eeb9f
joshuareisbord <joshuareisbord@gmail.com>
2021-07-01 10:05:54 -0700

    Doing a full refresh.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
 import React from 'react';
-import { Grid, makeStyles, Paper } from '@material-ui/core'
-import Divider from '@material-ui/core/Divider'
-import './App.css';
 
-import About from './components/about'
-import Projects from './components/projects'
-
-const useStyles = makeStyles((theme) => ({
-
-  root: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    flexDirection: 'column',
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-  },
-
-}));
-
-  export default function App(){
-
-    const classes = useStyles();
+function App(){
 
     return (
-      <div className={classes.root}>
-        <Grid container direction='column'justify='center' alignItems='center' spacing={6}>
-          <Grid item>
-            <About/>
-          </Grid>
-          <Grid item>
-            <Projects/>
-          </Grid>
-        </Grid>
-        
+      <div>
+        <h1>Something</h1>
       </div>
-      
-        
     );
 
-  }
+}
+
+export default App;
